c70183220b80100cf62387c4efa347ec587478e2
Elisha Cornelius <elishacornelius55@gmail.com>
2023-02-13 22:48:16 +0100

    changed interest to skills

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -4,7 +4,7 @@ import Github from "./github-logo.png"
 function Footer(){
     return(
         <div className="footer">
-            <a href="twitter.com/@changeastory"><img src={Twitter} alt="" className="twitter-icon"/></a>
+            <a href="https://twitter.com/ChangeAstory"><img src={Twitter} alt="" className="twitter-icon"/></a>
             <a href="https://github.com/ElishaCornelius"><img src={Github} alt="" className="twitter-icon"/></a>
         </div>
     )
--- src/Component/Interests.js
+++ src/Component/Interests.js
@@ -3,13 +3,14 @@ import React from "react"
 function Interests (){
     return(
         <div className="interests">
-            <h4 id="interests">interests</h4>
-            <p className="text2">
-                Over time i figured out that i see
-                 myself most times sorting and admiring graphics
-                  and user interfaces which is one important tool in 
-                  marketing and user experience.
-            </p>
+            <h4 id="interests">Programming Skills</h4>
+            <ul id="skill">
+                <li>React</li>
+                <li>CSS</li>
+                <li>Html</li>
+                <li>Javascript</li>
+                <li>Tailwind CSS</li>
+            </ul>
         </div>
     )
 }
