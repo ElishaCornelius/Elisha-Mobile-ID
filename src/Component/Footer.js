83ed21b3bb267607c1bd0f276f202de2a5a5efda
Elisha Cornelius <elishacornelius55@gmail.com>
2024-08-25 21:34:52 +0100

    changed twitter profile after the last one got suspended

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -4,7 +4,7 @@ import Github from "./github-logo.png"
 function Footer(){
     return(
         <div className="footer">
-            <a href="https://twitter.com/ChangeAstory"><img src={Twitter} alt="" className="twitter-icon"/></a>
+            <a href="https://x.com/Elisha_Kjnr"><img src={Twitter} alt="" className="twitter-icon"/></a>
             <a href="https://github.com/ElishaCornelius"><img src={Github} alt="" className="twitter-icon"/></a>
         </div>
     )
