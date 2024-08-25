cf138d64728a3d8a2eda584bd0f8b25e2a4f81a7
Elisha Cornelius <elishacornelius55@gmail.com>
2024-08-25 21:13:27 +0100

    changed alonf to along

--- src/Component/About.js
+++ src/Component/About.js
@@ -5,7 +5,7 @@ function About (){
         <div className="about">
             <h4 id="about">About</h4>
             <p className="text">1 yeat experienced ReactJs Developer with hands on experience 
-            in identifying web-based user interaction alonf with designing and 
+            in identifying web-based user interaction along with designing and 
             implementing highly responsive user interface components
             by deploying React concepts </p>
         </div>
